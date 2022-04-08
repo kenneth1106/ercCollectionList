c81bf3c7b1225afe269923596405e1d2b08df9b5
kenneth1106 <vivalavidagm@gmail.com>
2022-04-08 22:41:12 +0800

    Initial Commit

---
+++ ercCollectionList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ercCollectionList
+//
+//  Created by kenneth on 2022/4/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
